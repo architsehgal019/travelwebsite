8612a66348e2994e186ca1bd84496c1ed2e00048
gitikakhanna <gitikakhanna392@gmail.com>
2019-02-15 00:40:04 +0530

    site in progress

--- index.php
+++ index.php
@@ -2,12 +2,104 @@
 	<head>
 		<!-- bootstrap css link -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<!-- font awesome cdn -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 		<!-- link for css stylesheet -->
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<title>Kahi bhi ghum lo</title>
 	</head>
 	<body>
-		hello gitika
+		<div class="fluid-container">
+			
+			<!-- row for upper header -->
+			<div class="row bgpink text-white upper-header w-100">
+				<div class="col-lg-2 p-2 col-4 text-center offset-lg-2">
+					+91-7303969097
+				</div>
+				<div class="col-lg-3 p-2 text-center col-4">
+					travelwithjatin@gmail.com
+				</div>
+				<div class="col-lg-3 p-2 text-center col-4">
+					<a href="#"><img src="images/icons/fblogo.png" class="logo"></a>
+					<a href="#"><img src="images/icons/instalogo.png" class="logo ml-4"></a>
+					<a href="#"><img src="images/icons/twitterlogo.png" class="logo ml-4"></a>
+				</div>
+			</div>
+
+			<!-- row for main header -->
+			<div class="row bgblack navbar-row text-white w-100 main-header">
+					<div class="col-lg-2 offset-lg-1 col-12 p-2 d-flex align-items-center justify-content-between">
+						<a href="#" class="ml-3"><img src="images/icons/logo.png" class="site-logo"></a>
+						<button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#dropdownmenu" aria-controls="dropdownmenu" aria-expanded="false" aria-label="toggle navigation">
+							<i class="fa fa-bars"></i>
+						</button>
+					</div>
+					<div class="col-lg-6 col-12 d-flex align-items-center justify-content-center">
+						<nav class="navbar navbar-expand-lg">
+							<div class="collapse navbar-collapse mx-auto" id="dropdownmenu">
+								<ul class="navbar-nav mx-auto text-uppercase">
+									<li class="nav-item mx-3">
+										<a href="#" class="menu-items">home</a>
+									</li>
+									<li class="nav-item mx-3">
+										<a href="#" class="menu-items">destination</a>
+									</li>
+									<li class="nav-item mx-3">
+										<a href="#" class="menu-items">team</a>
+									</li>
+									<li class="nav-item mx-3">
+										<a href="#" class="menu-items">testimonial</a>
+									</li>
+									<li class="nav-item mx-3">
+										<a href="#" class="menu-items">pay</a>
+									</li>
+								</ul>
+							</div>
+						</nav>
+					</div>
+					<div class="col-lg-2 col-12 mb-1 d-flex align-items-center justify-content-center mx-2">
+						<div class="input-group">
+							<input type="text" class="form-control textfield" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2">
+							<div class="input-group-append">
+						    	<button class="btn" type="button"><i class="fa fa-search"></i></button>
+						  	</div>
+						</div>
+						<a href="#"><img src="images/icons/honeymoonlogo.png" class="honeymoon-logo ml-4"></a>
+					</div>
+			</div>
+			
+			<!-- image slider -->
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  				<div class="carousel-inner">
+    				<div class="carousel-item active">
+      					<img class="d-block w-100" src="images/sliderimg1.jpg" alt="First slide">
+    				</div>
+    				<div class="carousel-item">
+      					<img class="d-block w-100" src="images/sliderimg1.jpg" alt="Second slide">
+    				</div>
+    				<div class="carousel-item">
+      					<img class="d-block w-100" src="images/sliderimg1.jpg" alt="Third slide">
+    				</div>
+  				</div>
+  				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    				<span class="sr-only">Previous</span>
+  				</a>
+  				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+    				<span class="sr-only">Next</span>
+  				</a>
+			</div>
+
+			<!-- about us section -->
+			<div class="row bgblack text-white">
+				<div class="col-12 col-lg-6 p-4">
+					<h2 class="about-heading">KBG Holidays Pvt. Ltd.</h2>
+				</div>
+				<div class="col-12 col-lg-6">
+				</div>
+			</div>
+		</div>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
