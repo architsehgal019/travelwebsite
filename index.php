de679e4a12f15dcebcc2ec3f6b73478493549fdf
gitikakhanna <gitikakhanna392@gmail.com>
2019-02-15 14:17:13 +0530

    major structure formed

--- index.php
+++ index.php
@@ -93,13 +93,136 @@
 
 			<!-- about us section -->
 			<div class="row bgblack text-white">
-				<div class="col-12 col-lg-6 p-4">
-					<h2 class="about-heading">KBG Holidays Pvt. Ltd.</h2>
+				<div class="col-12 col-lg-6 p-5 mt-4">
+					<h1 class="about-heading">KBG Holidays Pvt. Ltd.</h1>
+					<br>
+					<p>
+						The holiday packages at KBG Holidays Pvt Ltd are designed to meet the different travelling needs of our customers from across the globe. Our experienced team provides you unbiased guidance at each step-undedrstanding your need, planning, traveling, staying and eploring. To make these packages more valuable, we constantly monitor and enhance them to meet the ever-changing environment and facilities.
+					</p>
+					<p>
+						Our goal is to save your time and money with quick, quality results and simple comparisons. Not only do we want to help you find the perfect trip- we want to make sure you are happy with the trip provided. We make sure that we customize your trip as per your requirements and catter to them at every step of your trip.
+					</p>
+					<p>
+						In simple words, "Your dream it & we picturize it in reality". As we stand only one thing.
+					</p>
+					<br>
+					<button class="btn btnsubmit float-right" type="submit">Read More</button>
 				</div>
-				<div class="col-12 col-lg-6">
+				<div class="col-12 col-lg-6 mt-4">
 				</div>
 			</div>
+			<hr class="divider">
+
+			<!-- popular destinations section -->
+			<div class="row bgblack p-5 text-center text-white d-flex justify-content-center">
+				<br>
+				<h1 class="about-heading">Popular Destinations</h1>
+				<div id="carouselExample" class="carousel slide w-100" data-ride="carousel">
+					<div class="carousel-inner">
+	    				<div class="carousel-item active">
+	      					<div class="row p-5">
+	      						<div class="col-4 destination">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      						<div class="col-4 destination">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      						<div class="col-4 destination">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      						<div class="col-4 destination mt-2">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      						<div class="col-4 destination mt-2">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      						<div class="col-4 destination mt-2">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      					</div>
+	    				</div>
+	    				<div class="carousel-item">
+	      					<div class="row p-5">
+	      						<div class="col-4 destination">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      						<div class="col-4 destination">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      						<div class="col-4 destination">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      						<div class="col-4 destination mt-2">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      						<div class="col-4 destination mt-2">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      						<div class="col-4 destination mt-2">
+	      							<img src="images/travel.jpg">
+	      							<div class="overlay">
+	      								<div class="text">Travel</div>
+	      							</div>
+	      						</div>
+	      					</div>
+	    				</div>
+  					</div>
+  					<div class="row d-flex align-items-center justify-content-center">
+  						<a href="#carouselExample" role="button" data-slide="prev">
+	    					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    					<span class="sr-only">Previous</span>
+  						</a>
+  						<a href="#carouselExample" role="button" data-slide="next" class="ml-3">
+	    					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    					<span class="sr-only">Next</span>
+  						</a>
+  					</div>
+				</div>
+			</div>
+
+			<hr class="divider">
+
+			<!-- archit your code starts from here -->
+
+
+
 		</div>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
