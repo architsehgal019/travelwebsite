42e08827e1d80c2c1e65f75030821e74dcaeee1f
gitikakhanna <gitikakhanna392@gmail.com>
2019-02-18 12:23:23 +0530

    honeymoon logo changed

--- index.php
+++ index.php
@@ -6,20 +6,19 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 		<!-- link for css stylesheet -->
 		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<title>Kahi bhi ghum lo</title>
+		<title>Kahi bhi ghoomlo</title>
 	</head>
 	<body>
-		<div class="fluid-container">
-			
+		<div class="fluid-container">	
 			<!-- row for upper header -->
-			<div class="row bgpink text-white upper-header w-100">
-				<div class="col-lg-2 p-2 col-4 text-center offset-lg-2">
-					+91-7303969097
+			<div class="row bgpink text-white upper-header fix-pos w-100">
+				<div class="col-4 p-2 text-center d-flex align-items-center justify-content-center">
+					<a href="tel:+91-7303969097">+91-7303969097</a>
 				</div>
-				<div class="col-lg-3 p-2 text-center col-4">
-					travelwithjatin@gmail.com
+				<div class="col-4 p-2 text-center d-flex align-items-center justify-content-center">
+					<a href="mailto:travelwithjatin@gmail.com">travelwithjatin@gmail.com</a>
 				</div>
-				<div class="col-lg-3 p-2 text-center col-4">
+				<div class="col-4 p-2 text-center">
 					<a href="#"><img src="images/icons/fblogo.png" class="logo"></a>
 					<a href="#"><img src="images/icons/instalogo.png" class="logo ml-4"></a>
 					<a href="#"><img src="images/icons/twitterlogo.png" class="logo ml-4"></a>
@@ -37,12 +36,9 @@
 					<div class="col-lg-6 col-12 d-flex align-items-center justify-content-center">
 						<nav class="navbar navbar-expand-lg">
 							<div class="collapse navbar-collapse mx-auto" id="dropdownmenu">
-								<ul class="navbar-nav mx-auto text-uppercase">
+								<ul class="navbar-nav mx-auto text-uppercase main-ul">
 									<li class="nav-item mx-3">
-										<a href="#" class="menu-items">home</a>
-									</li>
-									<li class="nav-item mx-3">
-										<a href="#" class="menu-items">destination</a>
+										<a href="#" class="menu-items">Destinations</a>
 									</li>
 									<li class="nav-item mx-3">
 										<a href="#" class="menu-items">team</a>
@@ -57,28 +53,25 @@
 							</div>
 						</nav>
 					</div>
-					<div class="col-lg-2 col-12 mb-1 d-flex align-items-center justify-content-center mx-2">
+					<div class="col-lg-3 col-12 mb-1 d-flex align-items-center justify-content-center">
 						<div class="input-group">
 							<input type="text" class="form-control textfield" placeholder="Search" aria-label="Search" aria-describedby="basic-addon2">
 							<div class="input-group-append">
 						    	<button class="btn" type="button"><i class="fa fa-search"></i></button>
 						  	</div>
 						</div>
-						<a href="#"><img src="images/icons/honeymoonlogo.png" class="honeymoon-logo ml-4"></a>
+						<a href="#"><img src="images/icons/honeymoonnewlogo.png" class="honeymoon-logo"></a>
 					</div>
 			</div>
 			
 			<!-- image slider -->
 			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   				<div class="carousel-inner">
-    				<div class="carousel-item active">
-      					<img class="d-block w-100" src="images/sliderimg1.jpg" alt="First slide">
+    				<div class="carousel-item active parallax">
     				</div>
-    				<div class="carousel-item">
-      					<img class="d-block w-100" src="images/sliderimg1.jpg" alt="Second slide">
+    				<div class="carousel-item parallax">
     				</div>
-    				<div class="carousel-item">
-      					<img class="d-block w-100" src="images/sliderimg1.jpg" alt="Third slide">
+    				<div class="carousel-item parallax">
     				</div>
   				</div>
   				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -94,7 +87,7 @@
 			<!-- about us section -->
 			<div class="row bgblack text-white">
 				<div class="col-12 col-lg-6 p-5 mt-4">
-					<h1 class="about-heading">KBG Holidays Pvt. Ltd.</h1>
+					<h2 class="about-heading">KBG Holidays Pvt. Ltd.</h2>
 					<br>
 					<p>
 						The holiday packages at KBG Holidays Pvt Ltd are designed to meet the different travelling needs of our customers from across the globe. Our experienced team provides you unbiased guidance at each step-undedrstanding your need, planning, traveling, staying and eploring. To make these packages more valuable, we constantly monitor and enhance them to meet the ever-changing environment and facilities.
@@ -105,10 +98,38 @@
 					<p>
 						In simple words, "Your dream it & we picturize it in reality". As we stand only one thing.
 					</p>
-					<br>
 					<button class="btn btnsubmit float-right" type="submit">Read More</button>
 				</div>
-				<div class="col-12 col-lg-6 mt-4">
+				<div class="col-12 col-lg-6 mt-4 p-5">
+					<div id="carouselIndicators" class="carousel slide" data-ride="carousel" style="height: 50%;">
+						<div class="carousel-inner h-100">
+						    <div class="carousel-item active h-100">
+						      <img class="d-block w-100 h-100" src="images/about1.jpg" alt="First slide">
+						    </div>
+						    <div class="carousel-item h-100">
+						      <img class="d-block w-100 h-100" src="images/about2.jpg" alt="Second slide">
+						    </div>
+						    <div class="carousel-item h-100">
+						      <img class="d-block w-100 h-100" src="images/about3.jpg" alt="Third slide">
+						    </div>
+						</div>
+					</div>
+					
+					<div class="carousel-indicators mt-3">
+						<div class="row">
+							<div data-target="#carouselIndicators" data-slide-to="0" class="active col-4">
+						    	<img src="images/about1.jpg">
+							</div>
+							    
+							<div data-target="#carouselIndicators" data-slide-to="1" class="col-4">
+							    <img src="images/about2.jpg">
+							</div>
+							    
+							<div data-target="#carouselIndicators" data-slide-to="2" class="col-4">
+							    <img src="images/about3.jpg">
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<hr class="divider">
@@ -116,7 +137,7 @@
 			<!-- popular destinations section -->
 			<div class="row bgblack p-5 text-center text-white d-flex justify-content-center">
 				<br>
-				<h1 class="about-heading">Popular Destinations</h1>
+				<h2 class="about-heading">Popular Destinations</h2>
 				
 				<div id="carouselExample" class="carousel slide w-100" data-ride="carousel">
 					<div class="carousel-inner">
@@ -125,37 +146,37 @@
 	      						<div class="col-4 destination">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      						<div class="col-4 destination">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      						<div class="col-4 destination">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      						<div class="col-4 destination mt-2">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      						<div class="col-4 destination mt-2">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      						<div class="col-4 destination mt-2">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      					</div>
@@ -165,37 +186,37 @@
 	      						<div class="col-4 destination">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      						<div class="col-4 destination">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      						<div class="col-4 destination">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      						<div class="col-4 destination mt-2">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      						<div class="col-4 destination mt-2">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      						<div class="col-4 destination mt-2">
 	      							<img src="images/travel.jpg">
 	      							<div class="overlay">
-	      								<div class="text">Travel</div>
+	      								<div class="text">Gujarat</div>
 	      							</div>
 	      						</div>
 	      					</div>
@@ -213,12 +234,149 @@
   					</div>
 				</div>
 			</div>
-
 			<hr class="divider">
 
-			<!-- archit your code starts from here -->
+			<!-- discount banner section --> 
+			<div class="row w-100 mt-5">
+                <div class="col-12 w-100 p-0">
+                    <img src="images/discount.jpg" alt="Discount" class="w-100">
+                </div>
+            </div>
 
+            <!-- google reviews section -->
+            <div class="row bgblack p-5 text-center text-white d-flex justify-content-center mt-5">
+				<br>
+				<h2 class="about-heading">Google Review</h2>
+				<div id="carouselExample" class="carousel slide w-100" data-ride="carousel">
+					<div class="carousel-inner">
+	    				<div class="carousel-item active">
+	      					<div class="row p-5">
+	      						<div class="col-5 destination">
+	      							<img src="images/travel.jpg">
+	      						</div>
+	      						<div class="col-2 destination">
+	      					    </div>
+	      						<div class="col-5 destination">
+	      							<img src="images/travel.jpg">
+	      						</div>
+	      					</div>
+	    				</div>
+	    				<div class="carousel-item">
+	      					<div class="row p-5">
+	      						<div class="col-5 destination">
+	      							<img src="images/travel.jpg">
+	      							
+	      						</div>
+	      						<div class="col-2 destination">
 
+	      						</div>
+	      						<div class="col-5 destination">
+	      							<img src="images/travel.jpg">
+	      							
+	      						</div>
+
+	      					</div>
+	    				</div>
+  					</div>
+  				</div>
+  				<div class="row d-flex align-items-center justify-content-center">
+  						<a href="#carouselExample" role="button" data-slide="prev">
+	    					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    					<span class="sr-only">Previous</span>
+  						</a>
+  						<a href="#carouselExample" role="button" data-slide="next" class="ml-3">
+	    					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    					<span class="sr-only">Next</span>
+  						</a>
+  					</div>
+			</div>
+			<hr class="divider">
+
+			<!-- contact us section -->
+			<div class="row mt-5 text-center bg-black text-white d-flex justify-content-center">
+                <br>
+                <div class="col-12">
+	                <h2 class = "about-heading">
+	                    Contact Us
+	                </h2>
+            	</div>
+                <br>   
+                <div class="col-6 mt-5 pl-4">
+                    <input class="form-control textfield w-50 element-center" type='text' name='name' placeholder="Name"  required>
+                    <input class="form-control textfield w-50 element-center" type='text' name='name' placeholder="Email"  required>
+                    <input class="form-control textfield w-50 element-center" type='text' name='name' placeholder="Contact No"  required>
+                </div>
+                <div class="col-6 mt-5">
+                    <textarea class="form-control textfield element-center w-50" rows = "3" placeholder="Message" required="" name="message"></textarea>
+                    <button class="btn btnsubmit contact-submit ml-3 mt-2" type="submit">SUBMIT</button>
+                </div>
+            </div>
+
+            <!-- map section -->
+            <div  class = "mt-5" style="width: 100%"><iframe width="100%" height="400" src="https://maps.google.com/maps?width=100%&amp;height=400&amp;hl=en&amp;q=District%20Center%20Janakpuri%2CDelhi+(Kahi%20Bhi%20Ghoomlo)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/map-my-route/">Plot a route map</a></iframe></div><br />
+
+            <!-- footer section -->
+            <div class="row d-flex bg-black text-center text-white align-items-center justify-content-center mt-5">
+            	<div class="col-3">
+              		<img src="images/icons/logo.png" class="bottom-logo">
+          		</div>
+          		<div class="col-6 text-center">
+		            <div class="row">
+		                <div class="col-12 ">
+		                    <div class="contact-list float-left">
+		                        <a href="#" class="list-item">About Us</a>
+		                        <a href="#" class="list-item ml-3 mr-3">|</a>
+		                        <a href="#" class="list-item">Why Us?</a>
+		                        <a href="#" class="list-item ml-3 mr-3">|</a>
+		                        <a href="#" class="list-item">Gallery</a>
+		                        <a href="#" class="list-item ml-3 mr-3">|</a>
+		                        <a href="#" class="list-item">T&C</a>
+		                        <a href="#" class="list-item ml-3 mr-3">|</a>
+		                        <a href="#" class="list-item">Contact-Us</a>
+		                    </div>  
+		                </div>
+		            </div>
+		            <div class="row">
+		                <div class="col-12 mt-4"> 
+		                  	<img src="images/icons/contactvlogo.png" class="contact-icons float-left">
+		                    <span class="float-left ml-3 icon-detail" style="margin-top: 1%;font-size: 16px;color: white;">travelwithjatin@gmail.com</span>
+		                </div>
+		            </div>
+		            <div class="row">
+		                <div class="col-12 mt-4">        
+		                  	<img src="images/icons/contactlogo.png" class="contact-icons float-left">
+		                    <span class="float-left ml-3 icon-detail" style="margin-top: 1%;font-size: 16px;color: white;">+91-7303969097</span>
+		                </div>
+		            </div>
+		            <div class="row">
+		                 <div class="col-12 mt-4">
+		                  	<img src="images/icons/address.png" class="contact-icons float-left">
+		                    <span class="float-left ml-3 icon-detail" style="margin-top: 1%;font-size: 16px;color: white;">506 B&c, Jaina Tower II,
+		                     District Centre Janakpuri, New Delhi 110085</span>
+		                </div>
+		            </div>
+          		</div>
+           
+	           	<div class="col-3">
+	               	<div class="row">
+	                   	<div class="col-12 social-upper"></div>
+	                   	<div class="col-12">
+	                       <img src="images/icons/fblogo.png" class="social-icon float-left">
+	                       <img src="images/icons/instalogo.png" class="social-icon float-left ml-5">
+	                       <img src="images/icons/twitterlogo.png" class="social-icon float-left ml-5">
+	                   	</div>
+	               </div>
+	           </div>
+         	</div>
+         
+          	<hr class="divider mt-3">
+          
+          	<div class="row d-flex text-center text-white align-items-center justify-content-center mt-3">
+              	<span>
+             		&copy; 2018 Kahin Bhi Ghoomlo. All rights reserved
+              	</span>  
+          	</div>
+            
 
 		</div>
 
--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 window.onscroll = function(){
-    if(window.pageYOffset > 50){
+    if(window.pageYOffset > 400){
         $(".main-header").addClass("scroll-fix");
+        $(".upper-header").removeClass("fix-pos");
     }
     else{
         $(".main-header").removeClass("scroll-fix");
+        $(".upper-header").addClass("fix-pos");
     }
 
 }
+
+$("ul.main-ul li.dropdown").hover(function(){
+    $(this).find("dropdown-menu").stop(true, true).delay(200).fadeIn(500);
+}, function(){
+    $(this).find(".dropdown-menu").stop(true, true).delay(200).fadeOut(500);
+});
