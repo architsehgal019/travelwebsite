45f3753137a6fe633a8d7a1054de85559353f370
gitikakhanna <gitikakhanna392@gmail.com>
2019-03-04 23:21:49 +0530

    changes saved

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
 	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- bootstrap css link -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<!-- font awesome cdn -->
@@ -58,16 +61,9 @@
 									</li>
 								</ul>
 								<button class="btn" type="button"><i class="fa fa-search"></i></button>
-<!--						<a href="#"><img src="images/icons/honeymoonnewlogo.png" class="honeymoon-logo"></a>-->
 							</div>
 						</nav>
 					</div>
-<!--
-					<div class="col-lg-2 col-12 mb-1 d-flex align-items-center justify-content-end">
-						<button class="btn" type="button"><i class="fa fa-search"></i></button>
-						<a href="#"><img src="images/icons/honeymoonnewlogo.png" class="honeymoon-logo"></a>
-					</div>
--->
 			</div>
 			
 			<!-- image slider -->
@@ -466,5 +462,23 @@
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
+
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135356945-1"></script>
+		<script>
+		  	window.dataLayer = window.dataLayer || [];
+		  	function gtag(){dataLayer.push(arguments);}
+		  	gtag('js', new Date());
+		  	gtag('config', 'UA-135356945-1');
+		</script>
+
+
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135356945-1"></script>
+		<script>
+		  	window.dataLayer = window.dataLayer || [];
+		  	function gtag(){dataLayer.push(arguments);}
+		  	gtag('js', new Date());
+		  	gtag('config', 'UA-135356945-1');
+		</script>
 	</body>
 </html>
