4b1babbb8c460b9c564a2e2783a73be0e09fae54
gitikakhanna <gitikakhanna392@gmail.com>
2019-03-05 00:29:59 +0530

    responsive upper header

--- index.php
+++ index.php
@@ -4,9 +4,11 @@ include('includes/config.php');
 
  ?>
 
-
 <html>
 	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- bootstrap css link -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<!-- font awesome cdn -->
@@ -22,7 +24,6 @@ include('includes/config.php');
 
 			<?php 
                 include('includes/header.php');
-
             ?>
 
 			<!-- image slider -->
