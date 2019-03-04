64d192e4a45e583d81db2b77ff7b6251d4bc39da
Archit Sehgal <archit_150@yahoo.co.in>
2019-03-04 22:08:40 +0530

    destination nav dynamic

---
+++ includes/footer.php
@@ -0,0 +1,60 @@
+<div class="row d-flex bg-black text-center text-white align-items-center justify-content-center mt-5">
+            	<div class="col-3">
+              		<img src="images/icons/logo.png" class="bottom-logo">
+          		</div>
+          		<div class="col-6 text-center">
+		            <div class="row">
+		                <div class="col-12 ">
+		                    <div class="contact-list float-left">
+		                        <a href="#" class="list-item">About Us</a>
+		                        <a href="#" class="list-item ml-3 mr-3">|</a>
+		                        <a href="#" class="list-item">Why Us?</a>
+		                        <a href="#" class="list-item ml-3 mr-3">|</a>
+		                        <a href="#" class="list-item">Gallery</a>
+		                        <a href="#" class="list-item ml-3 mr-3">|</a>
+		                        <a href="#" class="list-item">T&C</a>
+		                        <a href="#" class="list-item ml-3 mr-3">|</a>
+		                        <a href="#" class="list-item">Contact-Us</a>
+		                    </div>  
+		                </div>
+		            </div>
+		            <div class="row">
+		                <div class="col-12 mt-4"> 
+		                  	<img src="images/icons/contactvlogo.png" class="contact-icons float-left">
+		                    <span class="float-left ml-3 icon-detail" style="margin-top: 1%;font-size: 16px;color: white;">travelwithjatin@gmail.com</span>
+		                </div>
+		            </div>
+		            <div class="row">
+		                <div class="col-12 mt-4">        
+		                  	<img src="images/icons/contactlogo.png" class="contact-icons float-left">
+		                    <span class="float-left ml-3 icon-detail" style="margin-top: 1%;font-size: 16px;color: white;">+91-7303969097</span>
+		                </div>
+		            </div>
+		            <div class="row">
+		                 <div class="col-12 mt-4">
+		                  	<img src="images/icons/address.png" class="contact-icons float-left">
+		                    <span class="float-left ml-3 icon-detail" style="margin-top: 1%;font-size: 16px;color: white;">506 B&c, Jaina Tower II,
+		                     District Centre Janakpuri, New Delhi 110085</span>
+		                </div>
+		            </div>
+          		</div>
+           
+	           	<div class="col-3">
+	               	<div class="row">
+	                   	<div class="col-12 social-upper"></div>
+	                   	<div class="col-12">
+	                       <img src="images/icons/fblogo.png" class="social-icon float-left">
+	                       <img src="images/icons/instalogo.png" class="social-icon float-left ml-5">
+	                       <img src="images/icons/twitterlogo.png" class="social-icon float-left ml-5">
+	                   	</div>
+	               </div>
+	           </div>
+         	</div>
+         
+          	<hr class="divider mt-3">
+          
+          	<div class="row d-flex text-center text-white align-items-center justify-content-center mt-3">
+              	<span>
+             		&copy; 2018 Kahin Bhi Ghoomlo. All rights reserved
+              	</span>  
+          	</div>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+$(document).ready(function(){
 window.onscroll = function(){
     if(window.pageYOffset > 400){
         $(".main-header").addClass("scroll-fix");
         $(".upper-header").removeClass("fix-pos");
         $(".site-logo").css("height","65px");
         $(".site-logo").css("width","90px");
+        console.log("Scorlling");
     }
     else{
         $(".main-header").removeClass("scroll-fix");
@@ -12,10 +14,66 @@ window.onscroll = function(){
         $(".site-logo").css("width","120px");
     }
 
-}
+};
 
-$("ul.main-ul li.dropdown").hover(function(){
-    $(this).find("dropdown-menu").stop(true, true).delay(200).fadeIn(500);
-}, function(){
-    $(this).find(".dropdown-menu").stop(true, true).delay(200).fadeOut(500);
+//("ul.main-ul li.dropdown").hover(function(){
+//    $(this).find("dropdown-menu").stop(true, true).delay(200).fadeIn(500);
+//}, function(){
+//    $(this).find(".dropdown-menu").stop(true, true).delay(200).fadeOut(500);
+//});
+
+//$(".nav-item").on("hover",function(){
+//    alert("Hover");
+//    $(this).next().css("display","block");
+//    
+//});
+    
+       $( ".dropdown_submenu" ).hover(function() {
+        console.log("Hover");
+        $(".dropdown-menu").css("display","block");
+
 });
+    
+    $( ".dropdown-item" ).hover(function() {
+        console.log("Hover");
+        $(this).next().find(".dropdown-item-sub").css("display","block");
+////  $( this ).fadeOut( 100 );
+////  $( this ).fadeIn( 500 );
+});
+//    
+//    $( ".dropdown-item" ).mouseenter(function() {
+//   $(this).find("dropdown-item-sub").css("display","block");
+//  })
+   $( ".dropdown-item" ).mouseleave(function() {
+    $(".dropdown-item-sub").css("display","none");
+        console.log("Left");
+  });
+    
+    
+    
+    $('#tabs').on('click','.tablink,#prodTabs a',function (e) {
+    e.preventDefault();
+    var url = $(this).attr("data-url");
+
+    if (typeof url !== "undefined") {
+        var pane = $(this), href = this.hash;
+
+        // ajax load from data-url
+        $(href).load(url,function(result){      
+            pane.tab('show');
+        });
+    } else {
+        $(this).tab('show');
+    }
+});
+    
+    
+    
+    
+    
+    
+    
+    });
+
+
+
